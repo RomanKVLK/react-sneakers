d60938fa3bbfa784e70d5e93784a5709afac48d1
RomanKVLK <romnik20021994@gmail.com>
2022-10-18 21:36:44 +0300

    scss modules, onclick, props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Card from './components/Card/Card'
+import Card from './components/Card'
 import Header from './components/Header/Header';
 import Drawer from './components/Drawer/Drawer';
 
-const arr = [
-  { name: 'Мужские кроссовки Nike Blazer Mid Suede', price: 12999},
-  { name: 'Мужские Кроссовки Nike Air Max 270', price: 15600},
+const state = [
+  { title: 'Мужские кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg' },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: '/img/sneakers/2.jpg' },
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg' },
+  { title: 'Мужские Кроссовки Under Armour Curry 8', price: 15199, imageUrl: '/img/sneakers/5.jpg' },
+  { title: 'Мужские Кроссовки Nike Kyrie 7', price: 11299, imageUrl: '/img/sneakers/6.jpg' },
+  { title: 'Мужские Кроссовки Jordan Air Jordan 11', price: 10799, imageUrl: '/img/sneakers/7.jpg' },
+  { title: 'Мужские Кроссовки Nike LeBron XVIII', price: 16499, imageUrl: '/img/sneakers/8.jpg' },
+  { title: 'Мужские Кроссовки Nike Lebron XVIII Low', price: 13999, imageUrl: '/img/sneakers/9.jpg' },
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/10.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/2.jpg' },
+  { title: 'Мужские Кроссовки Nike Kyrie Flytrap IV', price: 11299, imageUrl: '/img/sneakers/3.jpg' },
+
 ]
 
 
@@ -22,8 +33,12 @@ function App() {
           </div>
         </div>
         <div className="card-row">
-            {arr.map((obj) => (
-            <Card />
+            {state.map((obj) => (
+            <Card 
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)} />
             ))}
         </div>
       </div>
