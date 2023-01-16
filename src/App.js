10521168938cf183971b49018c9dae2a58667953
RomanKVLK <romnik20021994@gmail.com>
2023-01-16 15:44:02 +0300

    g

--- src/App.js
+++ src/App.js
@@ -1,63 +1,84 @@
 import React from 'react';
-import axios from 'axios'
-import Card from './components/Card'
+import axios from 'axios';
+import Card from './components/Card';
 import Header from './components/Header/Header';
 import Drawer from './components/Drawer/Drawer';
 
-
 function App() {
+  console.log(1);
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://635eb78303d2d4d47af4dab0.mockapi.io/items').then(res => {
-      return res.json();
-    }).then(json => {
-      setItems(json);
-    });
+    fetch('https://635eb78303d2d4d47af4dab0.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
 
-    axios.get('https://635eb78303d2d4d47af4dab0.mockapi.io/items').then(res => setItems(res.data));
-    axios.get('https://635eb78303d2d4d47af4dab0.mockapi.io/cart').then(res => setCartItems(res.data))
+    axios
+      .get('https://635eb78303d2d4d47af4dab0.mockapi.io/items')
+      .then((res) => setItems(res.data));
+    axios
+      .get('https://635eb78303d2d4d47af4dab0.mockapi.io/cart')
+      .then((res) => setCartItems(res.data));
   }, []);
 
   const onAddToCart = (obj) => {
     axios.post('https://635eb78303d2d4d47af4dab0.mockapi.io/cart', obj);
-    setCartItems(prev => [...prev, obj])
-  }
+    setCartItems((prev) => [...prev, obj]);
+  };
 
   const onRemoveItem = (id) => {
     axios.delete(`https://635eb78303d2d4d47af4dab0.mockapi.io/cart/${id}`);
-    setCartItems(prev => prev.filter(item => item.id !== id));
-  }
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
 
   const onChangeSearchInput = (event) => {
-    setSearchValue(event.target.value)
-  }
+    setSearchValue(event.target.value);
+  };
 
   return (
     <div className="wrapper">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />
+      )}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex mb-40 align-center justify-between">
           <h1>{searchValue ? `Поиск по запросу "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block">
             <img src="/img/search.svg" alt="Search" />
-            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."></input>
-            {searchValue && <img onClick={() => setSearchValue('')} className="clear cu-p" src="/img/btn-remove.svg" alt="Remove"/>}
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder="Поиск..."></input>
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue('')}
+                className="clear cu-p"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            )}
           </div>
         </div>
         <div className="card-row">
-            {items.filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase())).map((item, index) => (
-            <Card
-              key={index} 
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onFavorite={() => console.log('Добавили в закладки')}
-              onPlus={onAddToCart} />
+          {items
+            .filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onFavorite={() => console.log('Добавили в закладки')}
+                onPlus={onAddToCart}
+              />
             ))}
         </div>
       </div>
