6105961b1881f1e9e0dc89e7ad0947090c185d78
RomanKVLK <romnik20021994@gmail.com>
2023-01-16 16:24:06 +0300

    cart fix

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,40 +1,69 @@
-function Drawer({onClose, onRemove, items = []}) {
-    return (
-        <div className="overlay">
-            <div className="drawer">
-            <h2 className="mb-30 d-flex justify-between mb-30">Корзина
-            <img onClick={onClose} className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove"/>
-            </h2>
-            <div className="items">
-                {items.map((obj) => (
-                    <div className="cartItem d-flex align-center mb-20">
-                    <img className="mr-20" width={70} height={70} src={obj.imageUrl} alt="Sneakers"/>
-                    <div className="mr-20">
-                        <p className="mb-5">{obj.title}</p>
-                        <b>{obj.price} руб.</b>
-                    </div>
-                    <img onClick={() => onRemove(obj.id)} className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-                    </div>
-                ))}
-            </div>
-            <div className="cartTotalBlock">
-                <ul>
-                <li className="d-flex">
-                    <span>Итого:</span>
-                    <div></div>
-                    <b>21 498 руб.</b>
-                </li>
-                <li className="d-flex">
-                    <span>Нолог 5%</span>
-                    <div></div>
-                    <b>1074 руб.</b>
-                </li>
-                </ul>
-                <button className="greenButton">Оформить заказ<img src="/img/arrow.svg" alt="Arrow" /></button>
-            </div>
-            </div>
+function Drawer({ onClose, onRemove, items = [] }) {
+  return (
+    <div className="overlay">
+      <div className="drawer">
+        <h2 className="mb-30 d-flex justify-between mb-30">
+          Корзина
+          <img
+            onClick={onClose}
+            className="removeBtn cu-p"
+            src="/img/btn-remove.svg"
+            alt="Remove"
+          />
+        </h2>
+        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+          <img
+            className="mb-20"
+            width="120px"
+            height="120px"
+            src="/img/box-empty.png"
+            alt="box-empty"
+          />
+          <h2>Корзина пустая</h2>
+          <p>Добавьте хотя бы пару кроссовок, чтобы сделать заказ</p>
+          <button className="greenButton">
+            <img src="/img/arrow-left.svg" alt="Arrow" />
+            Вернуться назад
+          </button>
         </div>
-    );
+        <div className="items">
+          {items.map((obj) => (
+            <div className="cartItem d-flex align-center mb-20">
+              <img className="mr-20" width={70} height={70} src={obj.imageUrl} alt="Sneakers" />
+              <div className="mr-20">
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price} руб.</b>
+              </div>
+              <img
+                onClick={() => onRemove(obj.id)}
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="cartTotalBlock">
+          <ul>
+            <li className="d-flex">
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li className="d-flex">
+              <span>Нолог 5%</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button className="greenButton">
+            Оформить заказ
+            <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Drawer;
